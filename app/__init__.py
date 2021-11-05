83ebf8d137361619d0108f03b0c3dcda930678eb
abdilawrence <lawrence.abdi@gmail.com>
2021-11-05 00:04:10 -0500

    Set up flask + more files, recreated demo code

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,5 @@
-import os
-from posix import ST_APPEND
-import sqlalchemy
-from yaml import load, Loader
-from Flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def init_connect_engine():
-    if os.environ.get('GAE_ENV') != 'standard':
-        variables = load(open("app.yaml"), Loader=Loader)
-        env_variables = variables['env_variables']
-        for var in env_variables:
-            os.environ[var] = env_variables[var]
-
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL(
-            drivername = "mysql+pymysql",
-            username=os.environ.get('MYSQL_USER'),
-            password=os.environ.get('MYSQL_PASSWORD'),
-            database=os.environ.get('MYSQL_DB'),
-            host = os.environ.get('MYSQL_HOST')
-        )
-    )
-    return pool
-
-db = init_connect_engine()
-
-conn = db.connect()
-results = conn.execute("SELECT * FROM Genres;").fetchall()
-print([x for x in results])
-conn.close()
-
 from app import routes
-
--- app/database.py
+++ app/database.py
@@ -1,79 +1,32 @@
-"""Defines all the functions related to the database"""
-from app import db
-
 def fetch_todo() -> dict:
-    """Reads all tasks listed in the todo table
-    Returns:
-        A list of dictionaries
-    """
 
-    conn = db.connect()
-    query_results = conn.execute("Select * from tasks;").fetchall()
-    conn.close()
-    todo_list = []
-    for result in query_results:
-        item = {
-            "id": result[0],
-            "task": result[1],
-            "status": result[2]
+    todo_list = [
+        {
+            "id": 1,
+            "task": "test1",
+            "status": "Todo"
+        },
+        {
+            "id": 2,
+            "task": "test1",
+            "status": "Todo"
         }
-        todo_list.append(item)
-
+    ]
     return todo_list
 
 
 def update_task_entry(task_id: int, text: str) -> None:
-    """Updates task description based on given `task_id`
-    Args:
-        task_id (int): Targeted task_id
-        text (str): Updated description
-    Returns:
-        None
-    """
-
-    conn = db.connect()
-    query = 'Update tasks set task = "{}" where id = {};'.format(text, task_id)
-    conn.execute(query)
-    conn.close()
+    pass
 
 
 def update_status_entry(task_id: int, text: str) -> None:
-    """Updates task status based on given `task_id`
-    Args:
-        task_id (int): Targeted task_id
-        text (str): Updated status
-    Returns:
-        None
-    """
-
-    conn = db.connect()
-    query = 'Update tasks set status = "{}" where id = {};'.format(text, task_id)
-    conn.execute(query)
-    conn.close()
+    pass
 
 
 def insert_new_task(text: str) ->  int:
-    """Insert new task to todo table.
-    Args:
-        text (str): Task description
-    Returns: The task ID for the inserted entry
-    """
-
-    conn = db.connect()
-    query = 'Insert Into tasks (task, status) VALUES ("{}", "{}");'.format(
-        text, "Todo")
-    conn.execute(query)
-    query_results = conn.execute("Select LAST_INSERT_ID();")
-    query_results = [x for x in query_results]
-    task_id = query_results[0][0]
-    conn.close()
-
-    return task_id
+    pass
+    return 1
 
 
 def remove_task_by_id(task_id: int) -> None:
-    """ remove entries based on task ID """
-    conn = db.connect()
-    query = 'Delete From tasks where id={};'.format(task_id)
-    conn.execute(query)
-    conn.close()
+    pass
